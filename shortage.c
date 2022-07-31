bc8ab20e325b9b508829d8a473b3c0fbb36f975c
Jahanzab-Jhandeer <74298308+Jahanzab-Jhandeer@users.noreply.github.com>
2022-07-31 15:10:20 +0500

    Add files via upload

---
+++ shortage.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// I am using ternery operator
+int main() {
+
+    int age;
+    printf("entr your age :");
+    scanf("%d" , age);
+
+age > 18 ? printf("adult\n") : printf("not adult\n");
+
+return 0;
+}
